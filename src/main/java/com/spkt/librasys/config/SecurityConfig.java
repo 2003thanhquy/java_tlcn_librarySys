e4deb92f1147d982fc881e746127db684cbc7e8b
2003thanhquy <quythanhhuynh2003@gmail.com>
2024-11-01 17:22:46 +0700

    filterChain public get token,not token -> OK

--- src/main/java/com/spkt/librasys/config/SecurityConfig.java
+++ src/main/java/com/spkt/librasys/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,6 +45,21 @@ public class SecurityConfig {
     private CustomJwtDecode customJwtDecode;
 
     @Bean
+    @Order(1) // Đảm bảo filter chain này được ưu tiên xử lý trước
+    public SecurityFilterChain publicFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(PUBLIC_GET_ENDPOINTS) // Áp dụng filter chain này cho các endpoint công khai
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+
+    @Bean
+    @Order(2) // Đảm bảo filter chain này được ưu tiên xử lý trước
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request->
                 request.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Cho phép tất cả các yêu cầu OPTIONS
